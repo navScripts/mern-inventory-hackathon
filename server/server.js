2e5dc24abf17108fb6e8b60814b0303ebd1b4167
Naveen <amnaveeng@gmail.com>
2025-07-24 19:50:11 +0530

    Set up Node.js/Express backend and MongoDB connection test

---
+++ server/server.js
@@ -0,0 +1,22 @@
+// server/index.js
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
